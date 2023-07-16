7670865e40200d1e1aab41b7a4efd4bcfd2e5e97
Hwangseoeun <sunny030921@gmail.com>
2023-07-16 22:39:00 +0900

    getter,setter 추가 수정

--- Backend/proj/src/main/java/com/example/proj/dto/UserRegisterDto.java
+++ Backend/proj/src/main/java/com/example/proj/dto/UserRegisterDto.java
@@ -10,4 +10,28 @@ public class UserRegisterDto {
     private String email;
     private String pw;
     private String nickName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
