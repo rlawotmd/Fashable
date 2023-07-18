bea463b60caba272fad63464f9ee1bb8d0ac23d3
jki12 <sjak0227@naver.com>
2023-07-18 19:37:14 +0900

    notice-list 파싱 기능 추가

--- Backend/proj/src/main/java/com/example/proj/Util.java
+++ Backend/proj/src/main/java/com/example/proj/Util.java
@@ -5,12 +5,16 @@ import org.jsoup.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Util {
     // ref - https://jsoup.org/
 
-    private static final String url = "https://en.wikipedia.org/wiki/Marmot";
+    private static final String url = "https://www.busan.go.kr/depart/index";
 
     public static void test() {
         parse(url);
@@ -20,15 +24,18 @@ public class Util {
         var conn = Jsoup.connect(url);
 
         try {
-            Document doc = conn.get();
+            Document document = conn.get();
 
-            if (doc.connection().response().statusCode() == 200) {
+            if (document.connection().response().statusCode() == 200) {
+                Elements notices = document.select(".notice-list > li > a"); // 공지사항^Big 이라고 적힌 내용 parse 했음.
+                List<String> titles = notices.eachText();
 
-                // parsing...
+                // hyper link랑 잘 해서 리턴.
 
+                for (var notice : titles) System.out.println(notice);
             }
 
-            System.out.println();
+            System.out.println("done");
 
         } catch (Exception e) {
             e.printStackTrace(); // logging.
